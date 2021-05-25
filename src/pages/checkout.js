6c2e80341ff0fa2efb2838b14b8e5b19ab0e1e07
ajeetjaiswal02 <ajeetjaiswal103@gmail.com>
2021-05-25 12:34:44 +0530

    update

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import Header from "../components/Header"
+import Image from "next/image";
 function Checkout() {
     return (
         <div className="bg-gray-100">
             <Header />
-            <h1>This is the Checkout Page</h1>
+            <main className="lg:flex max-w-screen-2xl mx-auto">
+                {/*Left* */}
+                <div className="flex-grow m-5 shadow-sm">
+                    <Image 
+                     src="https://links.papareact.com/ikj"
+                     width={1020}
+                     height={250}
+                     objectFit="contain"
+                    />
+                </div>
+                <div className="flex flex-col p-5 space-y-10 bg-white">
+                    <h1 className="text-3xl border-b pb-4">Your Shoping bag is Empty</h1>
+
+                </div>
+                {/*Right* */}
+            </main>
         </div>
     )
 }
